e72952f534eebc13daac076260929bc0fe7fec16
Mir Ali Talpur <hskalee123@gmail.com>
2018-11-20 13:54:19 -0800

    add sample spring boot app

---
+++ search-boot/src/main/java/com/searchboot/searchboot/SearchBootApplication.java
@@ -0,0 +1,12 @@
+package com.searchboot.searchboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchBootApplication.class, args);
+	}
+}
